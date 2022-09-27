79a40c1f7887f6be5b9ec9b383319b444a7c1601
Patrycja <53222738+PatrycjaGrabowska@users.noreply.github.com>
2022-09-27 19:31:55 +0200

    Update trim.py

--- MSc/trim.py
+++ MSc/trim.py
@@ -1,17 +1,17 @@
 from Bio import SeqIO
-for seq_record in SeqIO.parse(r"C:\Users\pati8\Desktop\ug\magisterka\program_do_wycinki\lambda_grupa_0.fasta", "fasta"):
-    print(seq_record.id) #zwraca numer w bazie
+for seq_record in SeqIO.parse(r"C:\fakepath\fasta_file.fasta", "fasta"):
+    print(seq_record.id)
     print('Długosc:')
-    print(len(seq_record)) #zwraca długoć sekwencji
+    print(len(seq_record)) 
     
     
     start_position, end_position = map(int, input('Podaj zakres:').split())
-    start_position = start_position - 1 # zmniejszenie o 1 ze wzgledu na to ze numerowanie zaczyna się od 0
+    start_position = start_position - 1 
     end_position = end_position - 1 
     final_size = print(start_position, end_position)
     my_sequence = seq_record.seq[start_position:end_position]
     
-    with open("trimmed_lambda_grupa_0.fasta", "a") as trimmed:
+    with open("trimmed_sequence.fasta", "a") as trimmed:
         identifier_line = ">" + seq_record.id + "\n"
         trimmed.write(str(identifier_line))
         trimmed.write(str(my_sequence + "\n"))
