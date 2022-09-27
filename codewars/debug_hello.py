e59f31291f4ff57f5eff0977f19629caeb85a49c
Patrycja <53222738+PatrycjaGrabowska@users.noreply.github.com>
2022-09-27 20:12:44 +0200

    Update debug_hello.py

--- codewars/debug_hello.py
+++ codewars/debug_hello.py
@@ -3,5 +3,5 @@ def say_hello(name):
     Debugging sayHello function. Prev. code:
     "Hello"
     """
-    return "Hello, " + name
+    return "Hello, there" + name
 print(say_hello('General Kenobi'))
